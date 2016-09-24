e3f44861e9834684eb90087a5aa0a5820ec406e8
Alex Slubsky <aslubsky@gmail.com>
2016-09-24 13:22:12 +0300

    redis connect

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on('connection', function (socket) {
     if (process.env.REDIS_URL) {
         var rtg = require("url").parse(process.env.REDIS_URL);
         var client = redis.createClient(rtg.port, rtg.hostname);
-        redis.auth(rtg.auth.split(":")[1]);
+        client.auth(rtg.auth.split(":")[1]);
     } else {
         var client = redis.createClient();
     }
