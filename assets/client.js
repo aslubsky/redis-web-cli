45822fc5aa413bd9f136f2cc5ffe078c5b3bad30
Alex Slubsky <aslubsky@gmail.com>
2016-10-16 10:46:11 +0300

    Update client.js

--- assets/client.js
+++ assets/client.js
@@ -1,4 +1,8 @@
 $(function () {
+    $('.play-btn').remove();
+    $('.play-code').each(function () {
+        $(this).append('<div title="Запустить код" class="play-btn"></div>');
+    });
     $('code.res').css('display', 'none');
     $('.play-btn').on('click', function () {
         var el = $(this).parent();
